8bed81bbe849ab764b64805ccddc4315df8ad481
Linus Kallin <jaglinus@gmail.com>
2020-02-20 14:28:19 +0100

    First

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$("a").click(function( event ) {
+    $("section").hide();
+    let target = $("a").index(event.target);
+    $($("section").get(target)).fadeIn(800);
+});
+
+$(document).ready(function(){
+    $(".line").animate({
+    width: '20vw'
+    }, 1200);
+  });
