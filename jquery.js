9edad43ed85e892a06e7adb006589c768cc92d2a
Linus Kallin <jaglinus@gmail.com>
2020-02-20 15:09:04 +0100

    final

--- jquery.js
+++ jquery.js
@@ -1,7 +1,7 @@
-$("a").click(function( event ) {
-    $("section").hide();
-    let target = $("a").index(event.target);
-    $($("section").get(target)).fadeIn(800);
+$("a:not(.mail)").click(function( event ) {
+        $("section").hide();
+        let target = $("a").index(event.target);
+        $($("section").get(target)).fadeIn(800);
 });
 
 $(document).ready(function(){
